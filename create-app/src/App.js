8f999bc75427747ddbcfdfdb6ba8e7bea74d2183
unknown <wroehrman@yahoo.com>
2018-09-29 13:17:44 -0500

    updates

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 // import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
+import Form from './components/Form';
 import './App.css';
 
 class App extends Component {
 
-  
+
 
   render() {
     return (
       <div>
         <Navbar />
+        <Form />
         <Wrapper />
 
       </div>
---
+++ create-app/src/components/Form/Form.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+    render() {
+        return (
+            <div class="container-form">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Name</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Phone #</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Location</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Example select</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>Towing</option>
+                            <option>Need Gas</option>
+                            <option>Need A Jumpstart</option>
+                            <option>Unlock Vehicle</option>
+                            <option>Change A Tire</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+export default Form;
