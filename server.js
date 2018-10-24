44b608b6afc25154b2b1271e72fe9858a5ef6de4
David Hoffmann <kc9gpj12@gmail.com>
2018-10-23 20:30:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ const user = require('./routes/user');
 const accountSid = 'AC9be4c10a22363fe6a4958f5f30b7ddc5'; 
 const authToken = 'a0839055e32cb426ce45dd1f41fad469'; 
 const client = require('twilio')(accountSid, authToken); 
-const uri = 'mongodb://localhost:27017/your-app-name'
+const uri = 'mongodb://heroku_t631vvjm:heroku_t631vvjm@ds137703.mlab.com:37703/heroku_t631vvjm'
 
 app.set("view engine");
 app.set("views", path.join(__dirname, "../client"));
 app.use(express.static(path.join(__dirname, "../client")));
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/FAST-project3";
+  process.env.MONGODB_URI || "mongodb://heroku_t631vvjm:heroku_t631vvjm@ds137703.mlab.com:37703/heroku_t631vvjm";
 
 mongoose.Promise = global.Promise;
 // mongoose.set('useNewUrlParser', true);
