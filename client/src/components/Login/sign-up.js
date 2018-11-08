613efbc08c308453a85be3f03973063e1c6a8f5b
unknown <wroehrman@yahoo.com>
2018-11-07 20:23:03 -0600

    update to sign up page

--- client/src/components/Login/sign-up.js
+++ client/src/components/Login/sign-up.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 
@@ -36,12 +37,12 @@ class Signup extends Component {
 				console.log(response)
 				if (!response.data.errmsg) {
 					console.log('successful signup')
-
-				} else {
-					console.log('username already taken')
 					this.setState({
 						redirectTo: '/login'
 					})
+				} else {
+					console.log('username already taken')
+
 				}
 			}).catch(error => {
 				console.log('signup error: ')
@@ -52,69 +53,73 @@ class Signup extends Component {
 
 
 	render() {
-		return (
-			<div className="SignupForm">
-				<h1>Sign up</h1>
-				<form className="form-horizontal">
-					<div className="form-group">
-						<div className="login-box">
-							<label className="form-label" htmlFor="username">Username</label>
+		if (this.state.redirectTo) {
+			return <Redirect to={{ pathname: this.state.redirectTo }} />
+		} else {
+			return (
+				<div className="SignupForm">
+					<h1>Sign up</h1>
+					<form className="form-horizontal">
+						<div className="form-group">
+							<div className="login-box">
+								<label className="form-label" htmlFor="username">Username</label>
+							</div>
+							<div className="window">
+								<input className="form-input"
+									type="text"
+									id="username"
+									name="username"
+									placeholder="Username"
+									value={this.state.username}
+									onChange={this.handleChange}
+								/>
+							</div>
 						</div>
-						<div className="window">
-							<input className="form-input"
-								type="text"
-								id="username"
-								name="username"
-								placeholder="Username"
-								value={this.state.username}
-								onChange={this.handleChange}
-							/>
+						<div className="form-group">
+							<div className="password-box">
+								<label className="form-label" htmlFor="password">Password: </label>
+							</div>
+							<div className="window">
+								<input className="form-input"
+									placeholder="password"
+									type="password"
+									name="password"
+									value={this.state.password}
+									onChange={this.handleChange}
+								/>
+							</div>
 						</div>
-					</div>
-					<div className="form-group">
-						<div className="password-box">
-							<label className="form-label" htmlFor="password">Password: </label>
+						<div className="form-group">
+							<div className="password-box">
+								<label className="form-label" htmlFor="admin-or-user">User or Admin: </label>
+							</div>
+							<div className="window">
+								<select
+									className="form-control"
+									id="options"
+									name="role"
+									value={this.state.role}
+									onChange={this.handleChange}
+								>
+									<option>Which App do you need? (Select One)</option>
+									<option>User</option>
+									<option>Administrator</option>
+								</select>
+							</div>
 						</div>
-						<div className="window">
-							<input className="form-input"
-								placeholder="password"
-								type="password"
-								name="password"
-								value={this.state.password}
-								onChange={this.handleChange}
-							/>
+						<div className="form-group ">
+							<div className="col-7"></div>
+							<button
+								className="btn btn-primary"
+								onClick={this.handleSubmit}
+								type="submit"
+							>Sign up</button>
 						</div>
-					</div>
-					<div className="form-group">
-						<div className="password-box">
-							<label className="form-label" htmlFor="admin-or-user">User or Admin: </label>
-						</div>
-						<div className="window">
-							<select
-								className="form-control"
-								id="options"
-								name="role"
-								value={this.state.role}
-								onChange={this.handleChange}
-							>
-								<option>Which App do you need? (Select One)</option>
-								<option>User</option>
-								<option>Administrator</option>
-							</select>
-						</div>
-					</div>
-					<div className="form-group ">
-						<div className="col-7"></div>
-						<button
-							className="btn btn-primary"
-							onClick={this.handleSubmit}
-							type="submit"
-						>Sign up</button>
-					</div>
-				</form>
-			</div>
+					</form>
+				</div>
 
-		)
+			)
+		}
 	}
 }
 
